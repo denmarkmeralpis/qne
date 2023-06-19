bced2a68b5b862edb6f68b564ede7e8d9b4195e2
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2023-06-19 13:27:54 +0800

    Implement stock finder

--- lib/stocks.rb
+++ lib/stocks.rb
@@ -2,12 +2,29 @@
 
 require_relative 'QNE/stock/all'
 require_relative 'QNE/stock/uoms'
+require_relative 'QNE/stock/find'
 
 module QNE
   class Stocks < Base
     include ::QNE::QueryBuilder
 
     attr_reader :id
+
+    def find(id)
+      finder = QNE::Stocks::Find.new(conn, id: id).call
+
+      return unless finder.success?
+
+      define_singleton_method 'uoms' do
+        find_uoms_by_id(id)
+      end
+
+      define_singleton_method 'show' do
+        @response = parse_to_json(finder.body)
+      end
+
+      self
+    end
 
     def all(params = {})
       @response = QNE::Stock::All.new(
