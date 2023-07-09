0794d9c1d7bc3e1189a351d63970748e6c65d4fa
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2023-07-10 00:43:22 +0800

    Support datatype based value for query params

--- lib/query_builder.rb
+++ lib/query_builder.rb
@@ -2,6 +2,16 @@
 
 module QNE
   module QueryBuilder
+    OPERATORS = {
+      eq: {
+        string: "COLUMN OPERATOR 'VALUE'",
+        default: "COLUMN OPERATOR VALUE",
+      },
+      in: {
+        default: "COLUMN OPERATOR (VALUE)"
+      }
+    }.freeze
+
     def eq_query(queries)
       filters = queries.map { |column, value| "#{column} eq '#{value}'" }
       filters.join(' and ')
@@ -10,8 +20,7 @@ module QNE
     def multi_query(queries)
       filters = queries.map do |column, operators|
         operators.map do |operator, value|
-          value = value.is_a?(String) ? "'#{value}'" : value
-          "#{column} #{operator} #{value}"
+          parse_query_by_operator(column, operator, value)
         end
       end
 
@@ -35,6 +44,19 @@ module QNE
     end
 
     private
+
+    def parse_query_by_operator(col, operator, val)
+      string = OPERATORS[operator.to_sym]
+      datatype = val.class.name.downcase.to_sym
+
+      return "#{col} #{operator} #{val}" unless string
+
+      query = String.new(string[datatype] || string[:default])
+
+      query.gsub!(/COLUMN/, col.to_s)
+      query.gsub!(/OPERATOR/, operator.to_s)
+      query.gsub!(/VALUE/, val)
+    end
 
     def filterables
       @filterables ||= {}
