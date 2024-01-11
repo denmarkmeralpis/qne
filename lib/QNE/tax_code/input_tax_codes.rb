62c3e83aeaa337b0e88f50dab3ef51a5816febb3
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2024-01-11 21:38:26 +0800

    Implement output/input tax codes

---
+++ lib/QNE/tax_code/input_tax_codes.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module QNE
+  module TaxCode
+    class InputTaxCodes
+      PATH = '/api/TaxCodes/InputTaxCodes'.freeze
+
+      def initialize(conn, params: {})
+        @conn = conn
+        @params = params
+      end
+
+      def call
+        @conn.get(PATH) do |req|
+          @params.each do |key, val|
+            req.params[key] = val
+          end
+        end
+      end
+    end
+  end
+end
--- lib/tax_codes.rb
+++ lib/tax_codes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'QNE/tax_code/all'
+require_relative 'QNE/tax_code/output_tax_codes'
+require_relative 'QNE/tax_code/input_tax_codes'
 
 module QNE
   class TaxCodes < Base
@@ -8,6 +10,22 @@ module QNE
 
     def all(params = {})
       @response = QNE::TaxCode::All.new(
+        conn, params: uri_query(params)
+      ).call
+
+      parse_to_json(@response.body)
+    end
+
+    def output_tax_codes(params = {})
+      @response = QNE::TaxCode::OutputTaxCodes.new(
+        conn, params: uri_query(params)
+      ).call
+
+      parse_to_json(@response.body)
+    end
+
+    def input_tax_codes(params = {})
+      @response = QNE::TaxCode::InputTaxCodes.new(
         conn, params: uri_query(params)
       ).call
 
